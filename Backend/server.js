57684f89ca111bf6e0778a52bfc19e7ebb3a6b69
ikozigbo <97337028+ikozigbo@users.noreply.github.com>
2023-08-14 11:37:57 +0100

    14/08 backend update

--- Backend/server.js
+++ Backend/server.js
@@ -16,7 +16,23 @@ const PORT = process.env.PORT || 5050;
 const app = express();
 
 // Enable CORS for all routes
-app.use(cors({ origin: "*" }));
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"], // Add the allowed methods here
+  })
+);
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "*");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization"); // Allow all methods
+  next();
+});
+
+// Handle preflight requests
+app.options("*", (req, res) => {
+  res.status(200).send();
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
